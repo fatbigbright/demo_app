f5ccf592079580c04fe3c1ebbd2dc1cef3425a80
Chang,Liang <bigbrightf@gmail.com>
2014-01-27 16:31:46 +0800

    Rspec unit test for static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+      it "should have content 'Sample App'" do
+          visit '/static_pages/home'
+          expect(page).to have_content('Sample App')
+      end
+  end
+
+  describe "Help page" do
+      it "should have content 'Help page'" do
+          visit '/static_pages/help'
+          expect(page).to have_content('Help page')
+      end
+  end
+
+  describe "About page" do
+      it "should have content 'About Us'" do
+          visit '/static_pages/about'
+          expect(page).to have_content('About Us')
+      end
+  end
+end
