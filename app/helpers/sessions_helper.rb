5ad11aa5ebc2939ce30237547c22388346075869
Chang,Liang <bigbrightf@gmail.com>
2014-03-17 10:31:45 +0800

    update 20140317 complete chapter 8.2.6

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
         !current_user.nil?
     end
 
+    def sign_out
+        self.current_user = nil
+        cookies.delete(:remember_token)
+    end
+
     # attr_accessor, just like setter/getter in C#
     def current_user=(user)
         @current_user = user
