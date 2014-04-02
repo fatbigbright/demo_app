3d6163ff27f4e306d35209cf23ffb4ed63fc49c8
Chang,Liang <bigbrightf@gmail.com>
2014-04-02 13:38:11 +0800

    update 20140402 exercises 9.6-1 refer: http://stackoverflow.com/questions/18305598/possible-bug-in-rails-4-tutorial-section-9-6-exercise-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,8 @@ class UsersController < ApplicationController
   end
   private 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password, 
+                                     :password_confirmation)
     end
     def signed_in_user
         unless signed_in?
