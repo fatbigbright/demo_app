59fb25bf326951a55eeaf38ce4ac6d852b446c36
Chang,Liang <bigbrightf@gmail.com>
2014-06-17 12:04:00 +0800

    update 20140617 complete chapter 11.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
-  resources :users
+  resources :users do
+      member do
+          get :following, :followers
+      end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # get "users/new" #-- not need anymore for 'resources :users' includes it
   root to: 'static_pages#home'
   match 'signup', to: 'users#new', via: 'get' #signup_path available after this route defined.
